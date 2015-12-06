939a20d944faac7b4aa3954e9cd67b60dcda3442
ReyPena <zonickblack@gmail.com>
2015-12-06 00:12:24 -0700

    Change in the server.js file, mongoUri.

--- core/server/server.js
+++ core/server/server.js
@@ -1,16 +1,17 @@
-var express = require("express"),
-cors = require("cors"),
-bodyParser = require("body-parser"),
-mongoose = require("mongoose"),
-passport = require("./service/passport"),
-app = express(),
-port = 8080;
+var express = require("express")
+  , cors = require("cors")
+  , bodyParser = require("body-parser")
+  , mongoose = require("mongoose")
+  , passport = require("./service/passport")
+  , mongoUri = "mongodb://localhost:27017/guitar-store"
+  , app = express()
+  , port = 8080;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname + "../client/public"));
+app.use(express.static("../client/public"));
 
-// mongoose.connect(mongoUri);
+mongoose.connect(mongoUri);
 mongoose.connection.once("open", function () {
   console.log("connected" + mongoUri);
 });
